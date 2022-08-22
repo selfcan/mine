2e397797cb84dd57269e3b40a26c3a3e1df0060a
selfcan <99958756+selfcan@users.noreply.github.com>
2022-08-22 19:02:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,14 +13,14 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.101/g' package/base-files/files/bin/config_generate
 # 修改内核版本
-#sed -i 's/KERNEL_PATCHVER:=5.18/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
-#sed -i 's/KERNEL_TESTING_PATCHVER:=5.18/KERNEL_TESTING_PATCHVER:=5.4/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.19/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.19/KERNEL_TESTING_PATCHVER:=5.4/g' target/linux/x86/Makefile
 add koolproxyR 
-git clone https://github.com/Beginner-Go/luci-app-koolproxyR.git package/luci-app-koolproxyR
+#git clone https://github.com/Beginner-Go/luci-app-koolproxyR.git package/luci-app-koolproxyR
 add luci-app-adbyby-plus
 #git clone https://github.com/tcsr200722/app-adbyby.git package/luci-app-adbyby-plus
 add luci-app-ikoolproxy
-git clone https://github.com/iwrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
+#git clone https://github.com/iwrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
 add luci-app-filebrowser
 git clone https://github.com/tcsr200722/luci-app-filebrowser.git package/luci-app-filebrowser
 add luci-app-dockerman
@@ -33,15 +33,15 @@ add openwrt-upx
 add luci-app-dnsfilter
 git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
 add luci-app-adblock-plus
-git clone https://github.com/zheshifandian/luci-app-adblock-plus.git package/luci-app-adblock-plus
+#git clone https://github.com/zheshifandian/luci-app-adblock-plus.git package/luci-app-adblock-plus
 add luci-app-jd-dailybonus
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 add luci-app-speedtest-web
-git clone https://github.com/tcsr200722/luci-app-speedtest-web.git package/luci-app-speedtest-web
+#git clone https://github.com/tcsr200722/luci-app-speedtest-web.git package/luci-app-speedtest-web
 add NetSpeedTest
 #git clone https://github.com/sirpdboy/NetSpeedTest package/luci-app-NetSpeedTest
 add luci-app-easymesh
-git clone https://github.com/tcsr200722/luci-app-easymesh.git package/luci-app-easymesh
+#git clone https://github.com/tcsr200722/luci-app-easymesh.git package/luci-app-easymesh
 add luci-app-diskman
 mkdir -p package/luci-app-diskman && \
 wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O package/luci-app-diskman/Makefile
@@ -50,7 +50,7 @@ wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Mak
 add luci-app-advanced
 #git clone https://github.com/tcsr200722/luci-app-advanced.git package/luci-app-advanced
 add luci-app-oaf
-git clone https://github.com/tcsr200722/OpenAppFilter.git package/luci-app-oaf
+#git clone https://github.com/tcsr200722/OpenAppFilter.git package/luci-app-oaf
 add luci-app-dockerman
 git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 add luci-app-gowebdav
@@ -60,9 +60,9 @@ add luci-app-aliyundrive-webdav
 add luci-app-jellyfin
 git clone https://github.com/tcsr200722/luci-app-jellyfin.git package/luci-app-jellyfin
 add luci-app-podminidlna
-git clone https://github.com/lisaac/luci-app-podminidlna.git package/luci-app-podminidlna
+#git clone https://github.com/lisaac/luci-app-podminidlna.git package/luci-app-podminidlna
 add luci-app-podsamba
-git clone https://github.com/lisaac/luci-app-podsamba.git package/luci-app-podsamba
+#git clone https://github.com/lisaac/luci-app-podsamba.git package/luci-app-podsamba
 add luci-app-alist
 git clone https://github.com/sbwml/openwrt-alist.git package/luci-app-alist
 add go-aliyun-webdav
@@ -73,6 +73,6 @@ git clone https://github.com/selfcan/luci-app-argon-config.git  package/luci-app
 add luci-app-aliyundrive-fuse
 git clone https://github.com/messense/aliyundrive-fuse.git package/luci-app-aliyundrive-fuse
 add luci-app-alist
-git clone https://github.com/selfcan/luci-app-alist.git package/luci-app-alist
+#git clone https://github.com/selfcan/luci-app-alist.git package/luci-app-alist
 ./scripts/feeds update -a
 ./scripts/feeds install -a
