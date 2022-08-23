1f0c89fce5b1caa6f18a8def6ccc612301f4f666
selfcan <99958756+selfcan@users.noreply.github.com>
2022-08-23 20:27:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -74,6 +74,6 @@ add luci-app-aliyundrive-fuse
 git clone https://github.com/messense/aliyundrive-fuse.git package/luci-app-aliyundrive-fuse
 add luci-app-alist
 #git clone https://github.com/selfcan/luci-app-alist.git package/luci-app-alist
-git clone https://github.com/fw876/helloworld.git package/luci-app-
+git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
 ./scripts/feeds update -a
 ./scripts/feeds install -a
