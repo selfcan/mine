7edfd9369b23042f26c0407b0b99f675563fe38f
selfcan <99958756+selfcan@users.noreply.github.com>
2022-12-22 16:00:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,31 +12,17 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.101/g' package/base-files/files/bin/config_generate
-add koolproxyR 
-#git clone https://github.com/Beginner-Go/luci-app-koolproxyR.git package/luci-app-koolproxyR
 add luci-app-adbyby-plus
 #git clone https://github.com/tcsr200722/app-adbyby.git package/luci-app-adbyby-plus
-add luci-app-ikoolproxy
-#git clone https://github.com/iwrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
 add luci-app-filebrowser
 git clone https://github.com/tcsr200722/luci-app-filebrowser.git package/luci-app-filebrowser
 add luci-app-dockerman
-#git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
-#git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
 add luci-app-argon-config
-#git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-add openwrt-upx
-#git clone https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 add luci-app-dnsfilter
 git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
-add luci-app-adblock-plus
-#git clone https://github.com/zheshifandian/luci-app-adblock-plus.git package/luci-app-adblock-plus
-add luci-app-jd-dailybonus
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-add luci-app-speedtest-web
-#git clone https://github.com/tcsr200722/luci-app-speedtest-web.git package/luci-app-speedtest-web
-add NetSpeedTest
-#git clone https://github.com/sirpdboy/NetSpeedTest package/luci-app-NetSpeedTest
 add luci-app-easymesh
 #git clone https://github.com/tcsr200722/luci-app-easymesh.git package/luci-app-easymesh
 add luci-app-diskman
@@ -44,8 +30,6 @@ mkdir -p package/luci-app-diskman && \
 wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O package/luci-app-diskman/Makefile
 mkdir -p package/parted && \
 wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
-add luci-app-advanced
-#git clone https://github.com/tcsr200722/luci-app-advanced.git package/luci-app-advanced
 add luci-app-oaf
 #git clone https://github.com/tcsr200722/OpenAppFilter.git package/luci-app-oaf
 add luci-app-dockerman
@@ -73,11 +57,5 @@ add luci-app-bypass
 # git clone https://github.com/kiddin9/openwrt-bypass.git package/luci-app-bypass
 add luci-app-alist
 git clone https://github.com/selfcan/luci-app-alist.git package/luci-app-alist
-add helloword
-# git clone https://github.com/fw876/helloworld.git package/helloworld
-add luci-app-adbyby-plus-lite
-git clone https://github.com/selfcan/luci-app-adbyby-plus-lite.git package/luci-app-adbyby-plus-lite
-add luci-app-homebox
-git clone https://github.com/selfcan/luci-app-homebox.git package/luci-app-homebox
 ./scripts/feeds update -a
 ./scripts/feeds install -a
