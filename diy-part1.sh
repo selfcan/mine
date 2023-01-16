e846a227e1bb7b82e0e7c57ea39d0e07fb5b1212
selfcan <99958756+selfcan@users.noreply.github.com>
2023-01-16 18:34:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,6 @@ sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefi
 
 # Add a feed source
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> "feeds.conf.default"
+echo "src-git small https://github.com/kenzok8/small" >> "feeds.conf.default"
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
