a924fb8003f0a591d09b76e07246eb09c20810e2
selfcan <99958756+selfcan@users.noreply.github.com>
2023-05-29 07:27:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # 更改默认IP地址
-sed -i 's/192.168.1.1/192.168.50.101/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
 # 清空登录密码
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 # 重新添加 luci-app-adbyby-plus-lite
