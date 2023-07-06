b1822f68f55530e1d0f007907554f410b9b4cf67
selfcan <99958756+selfcan@users.noreply.github.com>
2023-07-06 11:26:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -87,8 +87,8 @@ add luci-theme-kucat
 git clone https://github.com/selfcan/luci-theme-kucat.git package/luci-theme-kucat
 git clone https://github.com/uparrows/luci-theme-darkmatter.git package/luci-theme-darkmatter
 #rm -rf feeds/packages/net/ariang
-#rm -rf feeds/luci/applications/luci-app-aria2
+rm -rf feeds/luci/applications/luci-app-aria2
 #git clone https://github.com/sbwml/ariang-nginx package/ariang-nginx
-rm -rf package/lean/luci-app-aria2 && git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
+git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
 ./scripts/feeds update -a
 ./scripts/feeds install -a
