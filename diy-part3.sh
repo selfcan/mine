3c37a03474c325c1126d8910e0a48361c370d179
selfcan <99958756+selfcan@users.noreply.github.com>
2023-08-28 16:51:43 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,1 +1,25 @@
-1
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+sed -i "/helloworld/d" "feeds.conf.default"
+# Modify X86 Kernel 5.4
+#sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefile
+# Modify X86 Kernel 5.19
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
+
+# Add a feed source
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+#echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> "feeds.conf.default"
+#echo "src-git small https://github.com/kenzok8/small" >> "feeds.conf.default"
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo "src-git opentopd  https://github.com/sirpdboy/sirpdboy-package" >> "feeds.conf.default"
