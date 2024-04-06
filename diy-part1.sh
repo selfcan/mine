d2ac14ac763d38399abcf8d36a0e699a05376702
selfcan <99958756+selfcan@users.noreply.github.com>
2024-04-06 18:45:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,8 +19,8 @@ sed -i "/helloworld/d" "feeds.conf.default"
 
 # Add a feed source
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-#echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> "feeds.conf.default"
-#echo "src-git small https://github.com/kenzok8/small" >> "feeds.conf.default"
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> "feeds.conf.default"
+echo "src-git small https://github.com/kenzok8/small" >> "feeds.conf.default"
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo "src-git opentopd  https://github.com/sirpdboy/sirpdboy-package" >> "feeds.conf.default"
 
